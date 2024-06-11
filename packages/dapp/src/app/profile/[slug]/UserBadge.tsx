52393b1d741f5e7c9665d11be30a25c9793e6008
martines3000 <domajnko.martin@gmail.com>
2024-06-11 12:55:01 +0200

    fix: user badge card placement

--- packages/dapp/src/app/profile/[slug]/UserBadge.tsx
+++ packages/dapp/src/app/profile/[slug]/UserBadge.tsx
@@ -33,11 +33,14 @@ export const UserBadge = async ({
           <div className="w-full flex justify-center">
             <ProfileAvatar avatar={data.avatar} address={address} size="3xl" />
           </div>
-          <Card className="absolute h-18 text-sm w-full bottom-[-16px] p-2 hover:bg-gray-50 animated-transition overflow-hidden text-nowrap">
-            <div className="w-full flex flex-col justify-center items-center font-medium">
-              <div className="pb-1">
-                {data.displayName ?? formatAddress(address)}
-              </div>
+          <Card
+            className={cn(
+              'absolute h-18 text-sm w-full  p-2 hover:bg-gray-50 animated-transition overflow-hidden text-nowrap',
+              donationAmount ? 'bottom-[-16px]' : 'bottom-[4px]'
+            )}
+          >
+            <div className="w-full gap-y-[2px] flex flex-col justify-center items-center font-medium">
+              <div>{data.displayName ?? formatAddress(address)}</div>
               {donationAmount && (
                 <>
                   <div className="font-normal text-primary text-center text-xs">
@@ -70,7 +73,12 @@ export const UserBadgeSkeleton = ({
             )}
           />
         </div>
-        <Card className="absolute h-18 text-sm w-full bottom-[-16px] p-2 hover:bg-gray-50 animated-transition">
+        <Card
+          className={cn(
+            'absolute h-18 text-sm w-full  p-2 hover:bg-gray-50 animated-transition overflow-hidden text-nowrap',
+            !isEndorsementBadge ? 'bottom-[-16px]' : 'bottom-[4px]'
+          )}
+        >
           <div className="w-full flex flex-col gap-y-2 justify-center items-center font-medium">
             <Skeleton className="w-24 h-4 pb-1 rounded-full bg-primary-200" />
             {!isEndorsementBadge && (
