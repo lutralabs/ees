2dfebade28981fb7c379dc3b5a4759114c87ab87
martines3000 <domajnko.martin@gmail.com>
2024-06-11 20:42:14 +0200

    fix: handle `/api` routes

--- packages/dapp/src/middleware.ts
+++ packages/dapp/src/middleware.ts
@@ -6,22 +6,28 @@ const corsOptions = {
 };
 
 export function middleware(request: NextRequest) {
-  // Handle preflighted requests
-  const isPreflight = request.method === 'OPTIONS';
+  if (request.nextUrl.pathname.startsWith('/api')) {
+    // Handle preflighted requests
+    const isPreflight = request.method === 'OPTIONS';
 
-  if (isPreflight) {
-    const preflightHeaders = {
-      ...corsOptions,
-    };
-    return NextResponse.json({}, { headers: preflightHeaders });
+    if (isPreflight) {
+      const preflightHeaders = {
+        ...corsOptions,
+      };
+      return NextResponse.json({}, { headers: preflightHeaders });
+    }
+
+    // Handle simple requests
+    const response = NextResponse.next();
+
+    Object.entries(corsOptions).forEach(([key, value]) => {
+      response.headers.set(key, value);
+    });
+
+    return response;
   }
 
-  // Handle simple requests
   const response = NextResponse.next();
-
-  Object.entries(corsOptions).forEach(([key, value]) => {
-    response.headers.set(key, value);
-  });
 
   // Check path
   if (request.nextUrl.pathname === '/') {
