e5475a16496d2d7680eb90c383e7bbb056b2c079
martines3000 <domajnko.martin@gmail.com>
2024-06-11 13:45:34 +0200

    chore: add conract address for base network

--- packages/dapp/src/lib/contracts/addresses.ts
+++ packages/dapp/src/lib/contracts/addresses.ts
@@ -7,7 +7,7 @@ export const CONTRACT_ADDRESSES: { ees: { [key: number]: `0x${string}` } } = {
   ees: {
     1337: '0xa0b4b77fd7afd4a38810fb2c060f93af5e085156', // Localhost
     11155111: '0x306aa8b6640A4Ef12919Ed97b5d85c006DD68796', // Sepolia
-    8453: '0x', // Base
+    8453: '0x6BbcC7D0E661a4f5441F74Dc0Ab324ac4327D940', // Base
     84532: '0x63F610a03Caa82ca32386BDb6F447a93d4D6F6e7', // Sepolia Base
   },
 };
