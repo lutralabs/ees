69eee33fd9c079af496fec2104224d5816d50c0c
martines3000 <domajnko.martin@gmail.com>
2024-06-11 19:08:31 +0200

    chore: reduce caching to 1 day

--- packages/dapp/src/utils/appUrl.ts
+++ packages/dapp/src/utils/appUrl.ts
@@ -1,8 +1,10 @@
 const PRODUCTION_URL = process.env.NEXT_PUBLIC_APP_PRODUCTION_URL;
 const STAGING_URL = process.env.NEXT_PUBLIC_APP_STAGGING_URL;
+const DEVELOPMENT_URL = process.env.NEXT_PUBLIC_APP_DEVELOPMENT_URL;
 
 export const APP_URL =
   PRODUCTION_URL ||
   STAGING_URL ||
   process.env.VERCEL_URL ||
-  'http://localhost:3000';
+  DEVELOPMENT_URL ||
+  'https://endorse.fun';
