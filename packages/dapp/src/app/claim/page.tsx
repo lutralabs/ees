d0235a717444ef4a21a3db5c600432d75df636d6
Urban Vidovič <urbanfoundit@gmail.com>
2024-06-11 17:08:48 +0200

    fix: padding on claim page
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- packages/dapp/src/app/claim/page.tsx
+++ packages/dapp/src/app/claim/page.tsx
@@ -4,7 +4,7 @@ import { ClaimCard } from './ClaimCard';
 export default function Page() {
   return (
     <Container className="pt-16 max-w-[1440px]">
-      <div className="md:w-[70%] lg:w-[50%] sm:mt-24 mx-auto">
+      <div className="md:w-[70%] lg:w-[50%] mx-auto">
         <ClaimCard />
       </div>
     </Container>
