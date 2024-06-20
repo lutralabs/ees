3d73f9c7d8985a99f6baf0e6550efb13d2cb3787
Urban Vidovič <urbanfoundit@gmail.com>
2024-06-20 18:46:01 +0200

    feat: endorsee dialog keyboard nav
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- packages/dapp/src/components/Endorsee/EndorseeDialog.tsx
+++ packages/dapp/src/components/Endorsee/EndorseeDialog.tsx
@@ -4,28 +4,15 @@ import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { MagnifyingGlassIcon } from '@heroicons/react/20/solid';
-import { useState } from 'react';
+import { type KeyboardEventHandler, useState } from 'react';
 import { isAddress } from 'viem';
 import { useEndorsementStore } from '@/stores';
 import { PLATFORM_DATA, PlatformType } from '@/utils/platform';
 import { cn } from '@/lib/utils';
 import { useRouter } from 'next/navigation';
 import { MemoizedSVG } from '@/components/MemoizedSVG';
-
-const SUGGESTED_ACCOUNTS: { platform: PlatformType; value: string }[] = [
-  {
-    platform: PlatformType.ens,
-    value: 'vitalik.eth',
-  },
-  {
-    platform: PlatformType.lens,
-    value: 'stani.lens',
-  },
-  {
-    platform: PlatformType.farcaster,
-    value: 'jessepollak',
-  },
-];
+import { getSearchSuggestions, type SearchListItemType } from '@/utils';
+import { SUGGESTED_ACCOUNTS } from '@/utils/constants';
 
 export function EndorseeDialog() {
   const router = useRouter();
@@ -41,16 +28,82 @@ export function EndorseeDialog() {
   // Local state
   const [query, setQuery] = useState('');
   const [open, setOpen] = useState(false);
-  const [isEthereumAddress, setIsEthereumAddres] = useState(false);
+  const [suggestions, setSuggestions] =
+    useState<SearchListItemType[]>(SUGGESTED_ACCOUNTS);
+  const [activeIndex, setActiveIndex] = useState(-1);
+
+  const onKeyDown: KeyboardEventHandler = (e: {
+    keyCode: number;
+    shiftKey: any;
+    preventDefault: () => void;
+  }) => {
+    // enter
+    if (e.keyCode === 13) {
+      const _value = suggestions[activeIndex]
+        ? suggestions[activeIndex].label
+        : query.replaceAll('。', '.');
+      selectUser(suggestions[activeIndex].key, _value);
+    }
+    // escape
+    if (e.keyCode === 27) {
+      if (activeIndex === -1) {
+        setSuggestions([]);
+      } else {
+        setActiveIndex(-1);
+      }
+    }
+    // up
+    if (e.keyCode === 38 || (e.shiftKey && e.keyCode === 9)) {
+      if (suggestions?.length) e.preventDefault();
+      if (suggestions && suggestions.length === 1) {
+        setActiveIndex(0);
+        return;
+      }
+      if (!activeIndex) {
+        setActiveIndex(suggestions.length - 1);
+      } else {
+        setActiveIndex(activeIndex - 1);
+      }
+    }
+    // down
+    if (e.keyCode === 40 || (!e.shiftKey && e.keyCode === 9)) {
+      if (suggestions?.length) e.preventDefault();
+      if (suggestions && suggestions.length === 1) return setActiveIndex(0);
+      if (activeIndex === null || activeIndex >= suggestions.length - 1) {
+        setActiveIndex(0);
+      } else {
+        setActiveIndex(activeIndex + 1);
+      }
+    }
+  };
 
   // Functions
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(event.target.value);
-    if (isAddress(event.target.value)) {
-      setIsEthereumAddres(true);
-    } else if (isEthereumAddress) {
-      setIsEthereumAddres(false);
+    const value = event.target.value;
+    setQuery(value);
+    if (value === '') {
+      setSuggestions(SUGGESTED_ACCOUNTS);
+      return;
     }
+    if (isAddress(value)) {
+      setSuggestions([
+        {
+          key: PlatformType.ethereum,
+          label: value,
+          icon: '/icons/icon-ethereum.svg',
+        },
+      ]);
+      return;
+    }
+    const suggestions = getSearchSuggestions(value);
+    setSuggestions(suggestions);
+  };
+
+  const selectUser = (platform: PlatformType, handle: string) => {
+    changePlatform(platform);
+    changeDisplayValue(handle);
+    router.push(`/?platform=${platform}&account=${handle}`);
+    setOpen(false);
   };
 
   return (
@@ -65,150 +118,47 @@ export function EndorseeDialog() {
           <div className="relative flex items-center">
             <MagnifyingGlassIcon className="absolute left-2 top-1/2 h-4 w-4 -translate-y-1/2 transform" />
             <Input
+              onKeyDown={onKeyDown}
               placeholder="Search ENS, Farcaster and Lens"
               value={query}
               onChange={handleInputChange}
               className="pl-8 rounded-b-none"
             />
           </div>
-          <div
-            className={cn(
-              'w-full p-2 bg-white rounded-2xl',
-              query !== '' && 'hidden'
-            )}
-          >
+          <div className={cn('w-full p-2 bg-white rounded-2xl')}>
             <div className="text-thin mb-2 text-gray-600 text-sm">
-              Suggested accounts
+              {query === '' ? 'Suggested accounts' : 'Results'}
             </div>
-            {SUGGESTED_ACCOUNTS.map((account) => {
-              return (
-                <Button
-                  key={account.value}
-                  onMouseDown={() => {
-                    changePlatform(account.platform);
-                    changeDisplayValue(account.value);
-                    router.push(
-                      `/?platform=${account.platform}&account=${account.value}`
-                    );
-                    setOpen(false);
-                  }}
-                  variant="ghost"
-                  className="w-full justify-start"
-                >
-                  <div className="flex gap-x-2 items-center">
-                    <MemoizedSVG
-                      fill={PLATFORM_DATA[account.platform].color}
-                      src={PLATFORM_DATA[account.platform].icon}
-                      width={20}
-                      height={20}
-                    />
-                    {account.value}
-                  </div>
-                </Button>
-              );
-            })}
-          </div>
-          <div
-            className={cn(
-              'w-full p-2 bg-white rounded-2xl',
-              query === '' && 'hidden'
-            )}
-          >
-            <div className="text-thin mb-2 text-gray-600 text-sm">Results</div>
             <div className="w-full">
-              {isEthereumAddress ? (
-                <Button
-                  onMouseDown={() => {
-                    changePlatform(PlatformType.ethereum);
-                    changeDisplayValue(query);
-                    router.push(
-                      `/?platform=${PlatformType.ethereum}&account=${query}`
-                    );
-                    setOpen(false);
-                  }}
-                  variant="ghost"
-                  className="w-full justify-start"
-                >
-                  <div className="flex gap-x-2 items-center">
-                    <MemoizedSVG
-                      fill={PLATFORM_DATA[PlatformType.ethereum].color}
-                      src={PLATFORM_DATA[PlatformType.ethereum].icon}
-                      width={20}
-                      height={20}
-                    />
-                    {query}
-                  </div>
-                </Button>
-              ) : (
+              {
                 <>
-                  <Button
-                    onMouseDown={() => {
-                      changePlatform(PlatformType.ens);
-                      changeDisplayValue(`${query}.eth`);
-                      router.push(
-                        `/?platform=${PlatformType.ens}&account=${query}.eth`
-                      );
-                      setOpen(false);
-                    }}
-                    variant="ghost"
-                    className="w-full justify-start"
-                  >
-                    <div className="flex gap-x-2 items-center">
-                      <MemoizedSVG
-                        fill={PLATFORM_DATA[PlatformType.ens].color}
-                        src={PLATFORM_DATA[PlatformType.ens].icon}
-                        width={20}
-                        height={20}
-                      />
-                      {query}.eth
-                    </div>
-                  </Button>
-                  <Button
-                    onMouseDown={() => {
-                      changePlatform(PlatformType.lens);
-                      changeDisplayValue(`${query}.lens`);
-                      router.push(
-                        `/?platform=${PlatformType.lens}&account=${query}.lens`
-                      );
-                      setOpen(false);
-                    }}
-                    variant="ghost"
-                    className="w-full justify-start"
-                  >
-                    <div className="flex gap-x-2 items-center">
-                      <MemoizedSVG
-                        fill={PLATFORM_DATA[PlatformType.lens].color}
-                        src={PLATFORM_DATA[PlatformType.lens].icon}
-                        width={20}
-                        height={20}
-                      />
-                      {query}.lens
-                    </div>
-                  </Button>
-                  <Button
-                    onMouseDown={() => {
-                      changePlatform(PlatformType.farcaster);
-                      changeDisplayValue(query);
-                      router.push(
-                        `/?platform=${PlatformType.farcaster}&account=${query}`
-                      );
-                      setOpen(false);
-                    }}
-                    variant="ghost"
-                    className="w-full justify-start"
-                  >
-                    <div className="flex gap-x-2 items-center">
-                      <MemoizedSVG
-                        fill={PLATFORM_DATA[PlatformType.farcaster].color}
-                        src={PLATFORM_DATA[PlatformType.farcaster].icon}
-                        width={20}
-                        height={20}
-                      />
-                      @{query}
-                    </div>
-                  </Button>
+                  {suggestions.map((suggestion, idx) => {
+                    return (
+                      <Button
+                        key={suggestion.icon}
+                        onMouseDown={() => {
+                          selectUser(suggestion.key, suggestion.label);
+                        }}
+                        variant="ghost"
+                        className={cn(
+                          'w-full justify-start hover:bg-slate-300',
+                          activeIndex === idx ? 'bg-slate-300' : ''
+                        )}
+                      >
+                        <div className="flex gap-x-2 items-center">
+                          <MemoizedSVG
+                            fill={PLATFORM_DATA[suggestion.key].color}
+                            src={PLATFORM_DATA[suggestion.key].icon}
+                            width={20}
+                            height={20}
+                          />
+                          {suggestion.label}
+                        </div>
+                      </Button>
+                    );
+                  })}
                 </>
-              )}
+              }
             </div>
           </div>
         </div>
