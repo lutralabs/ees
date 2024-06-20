0cb04a6293282985260cc5e7a29440f4e6069207
Urban Vidovič <urbanfoundit@gmail.com>
2024-06-20 18:44:58 +0200

    chore: reformat not found page
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- packages/dapp/src/app/profile/[slug]/not-found.tsx
+++ packages/dapp/src/app/profile/[slug]/not-found.tsx
@@ -2,14 +2,10 @@ import { Container } from '@/components/Container';
 
 export default function NotFound() {
   return (
-    <Container className="pt-16">
-      <div className="w-full">
-        <div className="text-center">
-          <h1 className="text-lg font-medium tracking-tight text-gray-900 sm:text-xl">
-            Profile not found
-          </h1>
-        </div>
-      </div>
+    <Container className="pt-16 w-full text-center">
+      <h1 className="text-lg font-medium tracking-tight text-gray-900 sm:text-xl">
+        Profile not found
+      </h1>
     </Container>
   );
 }
